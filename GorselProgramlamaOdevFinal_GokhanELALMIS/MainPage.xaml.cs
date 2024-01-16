f57a4a5142fa84e62e23ed13e64dbc274dee58ea
Gökhan Elalmış <gokhanelalmis0@gmail.com>
2024-01-16 23:33:07 +0300

    Update

--- GorselProgramlamaOdevFinal_GokhanELALMIS/MainPage.xaml.cs
+++ GorselProgramlamaOdevFinal_GokhanELALMIS/MainPage.xaml.cs
@@ -9,6 +9,24 @@ namespace GorselProgramlamaOdevFinal_GokhanELALMIS
             InitializeComponent();
             BindingContext = new MainViewModel();
         }
+
+        private async void OnHamburgerMenuClicked(object sender, EventArgs e)
+        {
+            var result = await DisplayActionSheet("Select Page", "Cancel", null, "Giriş", "Kurlar", "Hava Durumu", "Haberler", "Yapilacaklar", "Ayarlar");
+
+            if (result == "Giriş")
+                await Navigation.PushAsync(new MainPage());
+            else if (result == "Kurlar")
+                await Navigation.PushAsync(new KurlarPage());
+            else if (result == "Haberler")
+                await Navigation.PushAsync(new Haberler());
+            else if (result == "Hava Durumu")
+                await Navigation.PushAsync(new HavaDurumu());
+            else if (result == "Yapilacaklar")
+                await Navigation.PushAsync(new YapilacaklarPage());
+            else if (result == "Ayarlar")
+                await Navigation.PushAsync(new AyarlarPage());
+        }
     }
 
 }
